65bd2da75e23178577244d20a9a79e7953cd4584
wian <u19043512@tuks.co.za>
2022-10-20 15:17:49 +0200

    Updated MilitaryUnit.h and Squad.h

--- MilitaryUnit.h
+++ MilitaryUnit.h
@@ -1,9 +1,13 @@
 #ifndef MILITARYUNIT_H
 #define MILITARYUNIT_H
-
+#include "Order.h"
 class MilitaryUnit {
-public:
-	virtual MilitaryUnit* clone() = 0;
+	public:
+		virtual MilitaryUnit* clone() = 0;
+		virtual ~MilitaryUnit();
+		virtual bool isLeaf() = 0;
+	protected:
+		Order* orders;
 };
 
 #endif
--- Squad.h
+++ Squad.h
@@ -3,17 +3,20 @@
 
 #include "MilitaryUnit.h"
 #include "Cell.h"
+#include <vector>
 
 class Squad : public MilitaryUnit {
 
-private:
-    Cell* occupyingCell;
-    MilitaryUnit* members;
-    int Ammo;
+    private:
+        Cell* occupyingCell;
+        std::vector<MilitaryUnit*> members;
+        int Ammo;
 
-public:
-    void moveSquad();
-    void setOccupyingCell();
+    public:
+        void moveSquad();
+        void setOccupyingCell(Cell* c);
+        MilitaryUnit* clone();
+        bool isLeaf(){return false;};
 };
 
 
