b38de8dd0121bb7a38aac618a540eb6e0fefe974
wian <u19043512@tuks.co.za>
2022-10-22 10:43:25 +0200

    Added basic skeleton for squad movement algorithm

--- Squad.cpp
+++ Squad.cpp
@@ -3,9 +3,39 @@
 //
 
 #include "Squad.h"
+#include "Infantry.h"
+#include "Tank.h"
 void Squad::moveSquad()
 {
 
+    /*
+    
+        Algorithm to select cell goes here
+
+    */
+
+    // Calculate total resource consumption
+    int rationsConsumed = 0;
+    float fuelConsumed = 0;
+    std::vector<MilitaryUnit*>::iterator it = members.begin();
+    for (; it != members.end(); ++it) 
+    {
+        switch(((TeamMembers*)(*it))->getType())
+        {
+            case infantry: // Can change ration consuption based on cell type
+                rationsConsumed += ((Infantry*)(*it))->getRationConsumption();
+                break;
+            case tank: // Can change fuel consuption based on cell type
+                fuelConsumed += ((Tank*)(*it))->getFuelConsumption();
+                break;
+            default:
+        };
+    }
+
+    
+    // Consume resources
+    fuel -= fuelConsumed;
+    rations -= rationsConsumed;
 }
 
 void Squad::setOccupyingCell(Cell* c)
