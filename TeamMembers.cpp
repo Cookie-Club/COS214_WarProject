2d7a023a3c0f7429521fde2abf32225eac64bc7d
wian <u19043512@tuks.co.za>
2022-10-22 10:19:11 +0200

    Added method implementation to TeamMembers

--- TeamMembers.cpp
+++ TeamMembers.cpp
@@ -1,2 +1,17 @@
 
 #include "TeamMembers.h"
+
+void TeamMembers::setHealthpoints(int hp)
+{
+    this->healthpoints = hp;
+}
+
+bool TeamMembers::isLeaf()
+{
+    return true;
+}
+
+UnitType TeamMembers::getType()
+{
+    return this->type;
+}
--- TeamMembers.h
+++ TeamMembers.h
@@ -2,17 +2,18 @@
 #define TEAMMEMBERS_H
 
 #include "MilitaryUnit.h"
-
+enum UnitType { infantry, tank };
 class TeamMembers : public MilitaryUnit {
 
     public:
         virtual MilitaryUnit* clone() = 0;
         void setHealthpoints(int hp);
-        virtual bool isLeaf(){return true;};
+        virtual bool isLeaf();
+        UnitType getType();
     protected:
         int damage;
         int healthpoints;
-
+        UnitType type;
 };
 
 
