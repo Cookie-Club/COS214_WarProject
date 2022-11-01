af25e0445da5fc9f6e09f75fbef71a28cc7b87ed
Megan Hugo <bellvlekkie@gmail.com>
2022-11-01 11:58:31 +0200

    Added 2D Array of cells implementation in WorldMap.cpp
    Added mapSize attribute to WorldMap.h too

--- WorldMap.cpp
+++ WorldMap.cpp
@@ -1,13 +1,18 @@
 #include "WorldMap.h"
 
 WorldMap::WorldMap(int worldSize) {
-    // how to create a 2D array of objects
-//	for (int i = 0; i < worldSize; i++){
-//        for (int j = 0; j < worldSize; j++){
-//            this.grid[i][j] = new Cell();
-//        }
-//    }
 
+    this.mapSize = worldSize;
+    this.grid = new Cell **[worldSize];
+
+    for (int i = 0; i < worldSize; i++)
+    {
+        this.grid[i] = new Cell *[worldSize];
+        for (int j = 0; j < worldSize; j++)
+        {
+            this.grid[i][j] = new Cell();
+        }
+    }
 
     // need to add cell features
 }
