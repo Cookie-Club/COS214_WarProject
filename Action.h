026f0426853aad9243c80f5316749b94b554b078
Kaityss <kaitlynsookdhew@gmail.com>
2022-11-02 09:39:19 +0200

    Updated state (Action) component for state pattern

--- Action.h
+++ Action.h
@@ -1,19 +1,27 @@
+/**
+    \file Action.h
+    \class Action
+    \brief State class of the State Pattern
+    \details The Action class defines an interface for encapsulating the behaviour associated with a particular state,
+    either Aggressive or defensive, of the Squad (context of the state pattern)
+    State: state
+    \author Kaitlyn Sookdhew
+*/
 #ifndef ACTION_H
 #define ACTION_H
-#include "Participants.h"
+#include "Squad.h"
 #include <iostream>
-#include "Enumerations.h"
 using namespace std;
 
+enum ActionType {Aggressive, Defensive};
 
 class Action {
-    protected:
-        ActionType type;
-    public:
-        Action();
-        virtual int handleChange() = 0;
-        ActionType getType();
-        void setType(ActionType aT);
+protected:
+    ActionType type;
+public:
+    Action(ActionType);
+    virtual int handleChange(Squad* s) = 0;
+    ActionType getType();
 };
 
 #endif
